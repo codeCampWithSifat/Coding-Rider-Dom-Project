485e6920a313969274e7024c390b04667c2fdda0
codeCampWithSifat <sayedhossainsifat300@.com>
2024-09-25 23:50:17 +0600

    project 3 are done

---
+++ project-4/app.js
@@ -0,0 +1,7 @@
+/**
+ * Project Requirements:
+ * - Change the background color by generating random hex color by clicking a button
+ * - Also display the hex code to a disabled input field
+ * - Add a button to copy the color code
+ * - Add a toast message when copied
+ */
